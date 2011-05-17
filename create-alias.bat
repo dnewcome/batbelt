6b79108f28cb19182835c4d951bc2da130ae6171
dnewcome <djn125@yahoo.com>
2011-05-17 01:47:44 -0700

    updated create-alias and deploy-tool to print usage if invoked with no arguments.

--- create-alias.bat
+++ create-alias.bat
@@ -1,4 +1,4 @@
-:: Creat script to run tools and download from remote repository 
+:: Create script to run tools and download from remote repository 
 ::	if they don't exist locally
 ::	2010 Dan Newcome
 
@@ -6,6 +6,12 @@
 :: create-alias <shortcut-name> <package-path> <executable-name>
 
 :: <package-path> is relative to the location of the batbelt root
+@echo off
+if "%1x"=="x" goto :end
 
 echo @echo off > alias\%1.bat
 echo call weblaunch.bat %2 %3 %%* >> alias\%1.bat
+
+goto :eof
+:end
+echo usage: create-alias ^<name^> ^<package-path^> ^<executable-name^>
