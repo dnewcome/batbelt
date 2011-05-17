48a1a8375fbb4856ce8890e75b10b1b0b3071f26
dnewcome <djn125@yahoo.com>
2011-05-17 00:04:19 -0700

    updated weblaunch to support executable location in a subfolder under the main package folder. The first argument to weblaunch is a relative path to the executable starting at the package folder.

--- batbelt/weblaunch.bat
+++ batbelt/weblaunch.bat
@@ -15,9 +15,10 @@ call config.bat
 
 :: handle arguments - shift off to consume first two args to weblaunch since
 :: we don't want to pass them to the target executable via %*
-set appname=%1
+set appname_tmp=%1
+for /f "tokens=1 delims=\" %%a in ("%appname_tmp%") do set appname=%%a
 set executable=%2
-set apppath=%~dp0%appname%
+set apppath=%~dp0%appname_tmp%
 set scriptdir=%~dp0
 shift && shift
 
@@ -31,7 +32,7 @@ if exist "%apppath%" (
 	echo %appname% not installed - fetching
 ::mkdir "%apppath%"
 	%scriptdir%curl\curl.exe %url%/%appname%.zip --O %appname%.zip
-	%scriptdir%7-zip\7za.exe x -o"%apppath%" %appname%.zip
+	%scriptdir%7-zip\7za.exe x -o"%appname%" %appname%.zip
 	del %appname%.zip
 	start %executable% %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
