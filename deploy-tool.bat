707092e2e01d12e681d470224dfd09f34eba34c5
dnewcome <djn125@yahoo.com>
2011-05-19 01:17:04 -0700

    Fixed up what gets echoed to stdout in deploy-tool.

--- deploy-tool.bat
+++ deploy-tool.bat
@@ -1,10 +1,11 @@
 :: deploy a new optional tool out to the webserver
 :: usage: deploy-tool <local-path> <name>
-@if "%1x"=="x" goto end
+@echo off
+if "%1x"=="x" goto end
 
 call config.bat
 batbelt\7-zip\7za.exe a -r "%2".zip ".\%1\*"
 pscp "%2".zip %toolpath%
-
+goto :eof
 :end
-@echo usage: deploy-tool ^<local-path^> ^<name^>
+echo usage: deploy-tool ^<local-path^> ^<name^>
