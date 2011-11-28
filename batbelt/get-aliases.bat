fb2f42f5de068bf1f981db0884aa31306927082d
dnewcome <djn125@yahoo.com>
2011-11-28 03:39:25 -0800

    adding --no-directories to wget command when getting aliases - this puts aliases in the mai batbelt folder without url subfolders

--- batbelt/get-aliases.bat
+++ batbelt/get-aliases.bat
@@ -5,7 +5,7 @@ call config.bat
 
 set scriptdir=%~dp0
 
-"%scriptdir%wget\wget.exe" %url%/alias/ --recursive --level=1 --accept .bat
+"%scriptdir%wget\wget.exe" %url%/alias/ --recursive --no-directories --level=1 --accept .bat
 
 
 endlocal
