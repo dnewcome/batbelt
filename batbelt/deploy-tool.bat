a6aa604f84f29ac9b6851983b72151c1ba740b54
dnewcome <djn125@yahoo.com>
2011-11-28 03:39:45 -0800

    adding deploy tool

--- batbelt/deploy-tool.bat
+++ batbelt/deploy-tool.bat
@@ -1,11 +1,12 @@
 :: deploy a new optional tool out to the webserver
 :: usage: deploy-tool <local-path> <name>
+:: TODO: probably redundant to supply path and name, they should be the same
 @echo off
 if "%1x"=="x" goto end
 
 call config.bat
 7-zip\7za.exe a -r "%2".zip ".\%1\*"
-pscp "%2".zip %toolpath%
+pscp\pscp "%2".zip %toolpath%/packages
 goto :eof
 :end
 echo usage: deploy-tool ^<local-path^> ^<name^>
