8218d9ef5483a91f4136b0ab77f628f38d36a2c7
dnewcome <djn125@yahoo.com>
2012-06-22 17:29:54 -0700

    changing layout - bb commands are now called via bb.bat, and the individual commands live in commands/

--- batbelt/bb.bat
+++ batbelt/bb.bat
@@ -1,11 +1,21 @@
 @echo off
+if "%1x"=="x" goto :usage
+
+set command=%1
+shift
+call commands\%command% %1 %2 %3 %4 %5 %6 %7 %8 %9 
+goto :end
+
+:usage
 echo.
 echo Batbelt copyright 2010 Dan Newcome
 echo provided under the MIT license
 echo.
 echo edit config.bat to point to Batbelt repository
-echo use `get-aliases' to retrieve available command shortcuts
-echo use `create-alias' to create a new alias to a tool
-echo use `deploy-alias' to send a new alias to the depot
-echo use `deploy-tool' to create zip package and send a to the depot
-echo type `install' to install permanently and set path
+echo `bb get-aliases' to retrieve available command shortcuts
+echo `bb create-alias' to create a new alias to a tool
+echo `bb deploy-alias' to send a new alias to the depot
+echo `bb deploy-tool' to create zip package and send a to the depot
+echo `bb install' to install permanently and set path
+
+:end
